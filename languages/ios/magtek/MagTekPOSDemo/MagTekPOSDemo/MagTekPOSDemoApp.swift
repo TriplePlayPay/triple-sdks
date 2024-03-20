1cac463a9556378dce06a79c9aa98feb98ead458
Parker Brooks <jonparkerbrooks@gmail.com>
2024-03-20 17:26:56 -0500

    add the files

---
+++ languages/ios/magtek/MagTekPOSDemo/MagTekPOSDemo/MagTekPOSDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagTekPOSDemoApp.swift
+//  MagTekPOSDemo
+//
+//  Created by Parker on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct MagTekPOSDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
