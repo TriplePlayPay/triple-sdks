dc187dfc01bf8206d62fe6246d6a32b6f2b3e200
Parker Brooks <jonparkerbrooks@gmail.com>
2024-03-20 17:26:03 -0500

    restructure files to group magtek stuff together

--- languages/ios/MagTekSDK/MagTekSDK_iOS/MagTekSDK.h
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-
-FOUNDATION_EXPORT double MagTekSDKVersionNumber;
-FOUNDATION_EXPORT const unsigned char MagTekSDKVersionString[];
-
-#import <MagTekSDK/mtscra.h>
--- languages/ios/MagTekSDK/MagTekSDK_iOS_Simulator/MagTekSDK.h
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-
-FOUNDATION_EXPORT double MagTekSDK_iOS_SimulatorVersionNumber;
-FOUNDATION_EXPORT const unsigned char MagTekSDK_iOS_SimulatorVersionString[];
-
-#import <MagTekSDK/mtscra.h>
